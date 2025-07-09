bbdf1d63d891078f633501516171c9eaa667a715
kunj10 <kunjpatel91012@gmail.com>
2025-07-09 18:34:42 +0530

    post request

--- main.py
+++ main.py
@@ -1,13 +1,46 @@
 
 from fastapi import FastAPI, Path, HTTPException, Query
-import json
+from fastapi.responses import JSONResponse
 
-app  = FastAPI()
+import json
+from pydantic import BaseModel, computed_field
+from typing import List, Dict, Annotated  
+app = FastAPI()
+
+class Patient(BaseModel):
+    id: Annotated[str, Path(description="The ID of the patient to view")]
+    name: Annotated[str, Path(description="The name of the patient")]
+    city: Annotated[str, Path(description="The city of the patient")] 
+    age: int
+    gender: str
+    height: float
+    weight: float
+    
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        return round(self.weight / (self.height ** 2), 2)
+      
+    @computed_field
+    @property
+    def verdict(self) -> str:
+        if self.bmi < 18.5:
+            return "Underweight"
+        elif 18.5 <= self.bmi < 24.9:
+            return "Normal weight"
+        elif 25 <= self.bmi < 29.9:
+            return "Overweight"
+        else:
+            return "Obesity"
 
 def load_data():
   with open('patients.json', 'r') as f:
     data  = json.load(f)
   return data
+
+def save_data(data):
+  with open('patients.json', 'w') as f:
+    json.dump(data, f)
 
 @app.get("/")
 def  hello():
@@ -45,3 +78,23 @@ def sort(
   data = load_data()
   sorted_data = sorted(data.items(), key=lambda x: x[1][sort_by], reverse=(order == 'desc'))
   return sorted_data
+
+
+@app.post('/create')
+def create_patient(patient: Patient):
+
+    # load existing data
+    data = load_data()
+
+    # check if the patient already exists
+    if patient.id in data:
+        raise HTTPException(status_code=400, detail='Patient already exists')
+
+    # new patient add to the database
+    data[patient.id] = patient.model_dump(exclude=['id'])
+
+    # save into the json file
+    save_data(data)
+
+    return JSONResponse(status_code=201, content={'message':'patient created successfully'})
+
